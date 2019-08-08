89417366191fa7a88fbc615b2638cfb913378ec2
artnattpat <52275214+artnattpat@users.noreply.github.com>
2019-08-08 12:16:45 +0700

    Delete Config.py

--- Config.py
+++
@@ -1,3 +0,0 @@
-Channel_secret = 'c433556039b630918058626c200a10b7'
-Channel_access_token = 'iMQTYaxWQkUcijJ5Zc7EHM6w4dDk+MJFZx9xagdntDX91Y11LnU/Zwyf6XaRwj9WdTEyf5KishrU9wgCJ2zK6cj4CBK+AHD/x64PI2qUlGub8Xv2t1gCslUeAFMLzBpUKwzivK7ZK/hOFaOdwCSnXwdB04t89/1O/w1cDnyilFU='
-Basic_id= '@144xcbik'
